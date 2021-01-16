706eb50a188be9cebdb33420597f3a45dce54e59
Isac Canedo <32867124+isaccanedo@users.noreply.github.com>
2021-01-15 21:36:47 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-// Copyright 2019 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// +---------------------------+
+// | @author Isac Canedo       |
+// +---------------------------+
 
 import 'package:flutter/material.dart';
 
